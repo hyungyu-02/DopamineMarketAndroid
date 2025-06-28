199e73d955dbf3a8c9fe1f79d7bee5590dee4843
siusiubeom <siubeom200591ugmaie.com>
2025-06-29 00:48:18 +0900

    RoutineScreen UI 완성

--- app/src/main/java/com/myteam/hackathonapp/presentation/routines/RoutinesScreen.kt
+++ app/src/main/java/com/myteam/hackathonapp/presentation/routines/RoutinesScreen.kt
@@ -1,13 +1,16 @@
 package com.myteam.hackathonapp.presentation.routines
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -45,20 +48,26 @@ fun RoutinesScreen(modifier: Modifier = Modifier, navController: NavHostControll
 fun RoutineScreenContent(modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(horizontal = 17.dp)
-    ){
-        LazyRoutineComponent(
+            .fillMaxSize()
+            .background(color = Color(0xFFF6F6F9)),
+    ) {
+        Column(
             modifier = Modifier
-                .padding(top = 81.dp),
-            name = "오늘의 루틴"
-        )
-        LazyRoutineComponent(
-            modifier = Modifier
-                .padding(top = 26.dp),
-            name = "고정루틴"
-        )
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(horizontal = 17.dp)
+        ){
+            LazyRoutineComponent(
+                modifier = Modifier
+                    .padding(top = 24.dp),
+                name = "고정루틴"
+            )
+            LazyRoutineComponent(
+                modifier = Modifier
+                    .padding(top = 26.dp),
+                name = "오늘의 루틴"
+            )
+        }
     }
 }
 
