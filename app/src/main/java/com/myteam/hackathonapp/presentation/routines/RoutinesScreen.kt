fd3dd309bb89f45de689fdc2d98ad2c459f479a5
hyungyu-02 <yona20020209@gmail.com>
2025-06-28 20:43:53 +0900

    feat: scaffold 각 스크린으로

--- app/src/main/java/com/myteam/hackathonapp/presentation/routines/RoutinesScreen.kt
+++ app/src/main/java/com/myteam/hackathonapp/presentation/routines/RoutinesScreen.kt
@@ -2,15 +2,46 @@ package com.myteam.hackathonapp.presentation.routines
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import com.myteam.hackathonapp.core.component.HackathonTitle
+import com.myteam.hackathonapp.presentation.component.BottomNavigationBar
+import com.myteam.hackathonapp.presentation.component.HackathonFAB
+import com.myteam.hackathonapp.presentation.component.HackathonTopAppBar
 
 @Composable
-fun RoutinesScreen(modifier: Modifier = Modifier) {
+fun RoutinesScreen(modifier: Modifier = Modifier, navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            HackathonTopAppBar(
+                userName = "User10",
+                coinAmount = 10
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(navController)
+        },
+        floatingActionButton = {
+            HackathonFAB { /* Routines 전용 FAB 액션 */ }
+        }
+    ) { innerPadding ->
+        RoutineScreenContent(
+            modifier = modifier
+                .padding(innerPadding)
+                .consumeWindowInsets(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun RoutineScreenContent(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize(),
@@ -24,5 +55,5 @@ fun RoutinesScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun DiaryScreenPreview() {
-    RoutinesScreen()
+    RoutineScreenContent()
 }
